a7259fa78cd0d9a10bf6da7953a93a518b3891fa
Akshay Sharma <fxn769@gmail.com>
2022-02-28 22:50:23 +0530

    new ColorRow added

--- app/src/main/java/io/ak1/drawboxsample/ui/components/CustomViews.kt
+++ app/src/main/java/io/ak1/drawboxsample/ui/components/CustomViews.kt
@@ -1,30 +1,32 @@
 package io.ak1.drawboxsample.ui.components
 
 import android.widget.SeekBar
+import androidx.compose.animation.*
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.graphics.BlendModeColorFilterCompat
 import androidx.core.graphics.BlendModeCompat
 import io.ak1.drawbox.DrawController
 import io.ak1.drawboxsample.R
-import io.ak1.drawboxsample.data.local.arrayOfColors
 
 
 /**
@@ -32,34 +34,76 @@ import io.ak1.drawboxsample.data.local.arrayOfColors
  * https://ak1.io
  */
 
+
 @Composable
-fun ColorRow(isVisible: Boolean, clicked: (Color) -> Unit) {
-    if (isVisible) {
-        Column(
-            modifier = Modifier
-                .height(100.dp)
-                .fillMaxWidth(),
-            verticalArrangement = Arrangement.SpaceEvenly
-        ) {
-            Text(text = "Colors", modifier = Modifier.padding(12.dp, 0.dp, 0.dp, 0.dp))
-            LazyRow(
-                modifier = Modifier
-                    .fillMaxWidth()
-            ) {
-                items(arrayOfColors) { item ->
-                    Image(
-                        painter = ColorPainter(item),
-                        contentDescription = "hi",
-                        Modifier
-                            .padding(10.dp)
-                            .size(25.dp)
-                            .clip(
-                                CircleShape
-                            )
-                            .clickable {
-                                clicked.invoke(item)
+fun ColorRow(
+    isVisible: Boolean,
+    rowElementsCount: Int = 8,
+    colors: Array<Color>,
+    clickedColor: (Color) -> Unit
+) {
+    val density = LocalDensity.current
+    val defaultColor = remember {
+        mutableStateOf(colors[0])
+    }
+    AnimatedVisibility(
+        visible = isVisible,
+        enter = slideInVertically {
+            // Slide in from 40 dp from the top.
+            with(density) { -40.dp.roundToPx() }
+        } + expandVertically(
+            // Expand from the top.
+            expandFrom = Alignment.Top
+        ) + fadeIn(
+            // Fade in with the initial alpha of 0.3f.
+            initialAlpha = 0.3f
+        ),
+        exit = slideOutVertically() + shrinkVertically() + fadeOut()
+    ) {
+
+        var columnsSize: Int = colors.size / rowElementsCount
+        val remaining = colors.size % rowElementsCount
+        if (remaining > 0) {
+            columnsSize++
+        }
+        Column(modifier = Modifier.padding(16.dp, 8.dp,16.dp, 16.dp)) {
+            repeat(columnsSize) { column ->
+                println()
+                Row {
+                    repeat(rowElementsCount) { row ->
+                        val pos = (column * rowElementsCount) + row
+                        var size = 22.dp
+                        if (pos < colors.size) {
+                            val color = colors[pos]
+                            if (defaultColor.value == color) {
+                                size = 36.dp
                             }
-                    )
+
+                            IconButton(
+                                onClick = {
+                                    defaultColor.value = color
+                                    clickedColor(color)
+
+                                }, modifier = Modifier
+                                    .weight(1f, true)
+
+                            ) {
+                                Icon(
+                                    painterResource(id = R.drawable.ic_color),
+                                    contentDescription = stringResource(id = R.string.image_desc),
+                                    tint = color,
+                                    modifier = Modifier.size(size)
+                                    //.animateContentSize(animationSpec = tween(1000,100,LinearOutSlowInEasing))
+                                )
+                            }
+                        } else {
+                            Spacer(
+                                modifier = Modifier
+                                    .weight(1f, true)
+                            )
+                        }
+
+                    }
                 }
             }
         }
@@ -68,7 +112,7 @@ fun ColorRow(isVisible: Boolean, clicked: (Color) -> Unit) {
 
 @Composable
 fun ControlsBar(
-    drawController:DrawController,
+    drawController: DrawController,
     onDownloadClick: () -> Unit,
     onColorClick: () -> Unit,
     onSizeClick: () -> Unit,
@@ -145,7 +189,21 @@ fun CustomSeekbar(
     thumbColor: Int,
     onProgressChanged: (Int) -> Unit
 ) {
-    if (isVisible) {
+    val density = LocalDensity.current
+    AnimatedVisibility(
+        visible = isVisible,
+        enter = slideInVertically {
+            // Slide in from 40 dp from the top.
+            with(density) { -40.dp.roundToPx() }
+        } + expandVertically(
+            // Expand from the top.
+            expandFrom = Alignment.Top
+        ) + fadeIn(
+            // Fade in with the initial alpha of 0.3f.
+            initialAlpha = 0.3f
+        ),
+        exit = slideOutVertically() + shrinkVertically() + fadeOut()
+    ) {
         val context = LocalContext.current
         Column(
             modifier = Modifier
